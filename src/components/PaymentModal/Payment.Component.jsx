187320596799fb720e225fd9da2c2c5094491208
showre895 <ysreddy895@gmail.com>
2022-12-11 16:36:37 +0530

    Payment gateway complete

--- src/components/PaymentModal/Payment.Component.jsx
+++ src/components/PaymentModal/Payment.Component.jsx
@@ -9,7 +9,7 @@ const PaymentModel = ({ setIsOpen, isOpen, price }) => {
 
   const launchRazorPay = () => {
     let options = {
-      key: "rzp_test_Taf4Olo2TluDGc",
+      key: "rzp_test_0x2N9f9VOf3Eii",
       amount: price * 100,
       currency: "INR",
       name: "Book My Show Clone",
